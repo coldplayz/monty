392345d5319d5d9fda963e6ab6acef18bc3e42b7
scaarifintl <scaarifintl@gmail.com>
2022-10-06 07:43:42 +0300

    update opslist array

--- find_opfunc.c
+++ find_opfunc.c
@@ -14,10 +14,8 @@ opfunc find_opfunc(char *opstr)
 	/*oplist_t *oplist_cpy;*/
 	int i = 0;
 	instruction_t opslist[] = {
-		{"push", push},
-		{"pall", pall},
-		{"swap", swap},
-		{"pop", pop},
+		{"push", push}, {"pall", pall}, {"swap", swap}, {"pop", pop},
+		{"add", add}, {"pint", pint},
 		{NULL, NULL},
 	};
 
--- monty.m
+++ monty.m
@@ -3,9 +3,7 @@ pint
 push 2
 pint
 push 3
-<<<<<<< HEAD
 pint
-=======
 pall
 push 1
 push 2
@@ -13,4 +11,3 @@ push 3
 pall
 swap
 pall
->>>>>>> swap
