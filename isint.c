ddadefd93eeeb1589838a494153a1b6c648a03c1
greenbel <greenbel.chibuike@gmail.com>
2022-10-06 20:35:51 +0000

    HAndled int with signs.
           modified:   isint.c
           modified:   monty.m

--- isint.c
+++ isint.c
@@ -12,20 +12,27 @@
  */
 int isint(char *str)
 {
-	int i = 0;
+	int i = 0, entry_flag = 0;
 
-	if (str[0] == '-')
+	if (str[0] == '-' || str[0] == '+')
 	{
 		i = 1;
 	}
 
 	for (; str[i]; i++)
 	{
+		entry_flag = 1;
 		if (isdigit(str[i]) == 0)
 		{
 			/* str[i] not a digit char */
 			return (0);
 		}
+	}
+
+	if (!entry_flag)
+	{
+		/* loop was not entered */
+		return (0);
 	}
 
 	return (1);
